d0fd95c5aa33514c6b0a16a79c2d4131fdc27843
Sasha <safontsanek@gmail.com>
2026-05-28 12:57:14 +0200

    Comentarios y las foreign key explicadas

--- db_final.sql
+++ db_final.sql
@@ -1,4 +1,5 @@
 -- 1. USUARIOS
+-- Tabla para almacenar la información de los usuarios registrados en la plataforma
 CREATE TABLE usuarios (
     usuario_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(20) UNIQUE NOT NULL,
@@ -9,6 +10,7 @@ CREATE TABLE usuarios (
 );
 
 -- 2. CANALES
+--Cada usuario tiene su propio canal (perfil)
 CREATE TABLE canales (
     canal_id INT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT UNIQUE NOT NULL,
@@ -17,9 +19,11 @@ CREATE TABLE canales (
     logo_url VARCHAR(255),
     stream_key VARCHAR(100) UNIQUE NOT NULL, -- Clave para OBS
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id) ON DELETE CASCADE
+    --Hemos puesto foreign key de esto porque está relacionada la tabla de usuarios con canales. 1:1
 );
 
 --3. LIVE
+--Cada usuario puede hacer lives a través de su canal y ver los lives de otra gente 
 CREATE TABLE live (
     live_id INT AUTO_INCREMENT PRIMARY KEY,
     canal_id INT NOT NULL,
@@ -30,27 +34,35 @@ CREATE TABLE live (
     fin TIMESTAMP NULL,
     estado ENUM('en_vivo', 'finalizado') DEFAULT 'en_vivo',
     FOREIGN KEY (canal_id) REFERENCES canales(canal_id) ON DELETE CASCADE
+    --Esta foreign key conecta la ID del canal con la ID del directo. 1:1
 )
 
 -- 4. SEGUIDORES
+--Cada canal tiene sus seguidores
 CREATE TABLE seguidores (
     seguidor_id INT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT NOT NULL, -- Quien sigue
     canal_id INT NOT NULL,   -- Canal seguido
     UNIQUE(usuario_id, canal_id),
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id) ON DELETE CASCADE,
+    --Foreign key aquí hace la función de recopilar todos los seguidores del usuario N:1
     FOREIGN KEY (canal_id) REFERENCES canales(canal_id) ON DELETE CASCADE
+    --Tiene la misma función igual que anterior. N:1
 );
 
 -- 5. HISTORIAL (Registro de transmisiones pasadas)
+--Cada usuario tiene un historial
 CREATE TABLE historial (
     usuario_id INT NOT NULL,
     historial_id INT AUTO_INCREMENT PRIMARY KEY,
     canal_id INT NOT NULL,
     contenido TEXT NOT NULL,
     FOREIGN KEY (contenido) REFERENCES live(contenido) ON DELETE CASCADE,
+    --La foreign key va aquí porque cada contenido tendrá historial. N:1
     FOREIGN KEY (canal_id) REFERENCES canales(canal_id) ON DELETE CASCADE,
+    --Se guardará cada canal que ha visitado el usuario. N:1
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id) ON DELETE CASCADE
+    --Necesitamos ID de usuario para asignar internamente una página de sugerencias a cada usuario. 1:1
 );
 
 -- 6. CHAT LIVE (Mensajes en tiempo real en los directos)
@@ -58,6 +70,7 @@ CREATE TABLE chat_live (
     mensaje_live_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT NOT NULL,
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id) ON DELETE CASCADE
+    --Esta foreign key une el mensaje de cada usuario con chat. 1:1
 );
 
 -- 7. CHAT PRIVADO (Mensajería directa entre usuarios)
@@ -69,7 +82,9 @@ CREATE TABLE chat_privado (
     leido BOOLEAN DEFAULT FALSE,
     enviado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (remitente_id) REFERENCES usuarios(usuario_id) ON DELETE CASCADE,
+    --Esta foreign key une el mensaje de cada usuario con chat privado. 1:1
     FOREIGN KEY (destinatario_id) REFERENCES usuarios(usuario_id) ON DELETE CASCADE
+    --Esta foreign key une el mensaje de cada usuario con chat privado. 1:1
 );
 
 -- 8. TIENDA (Productos digitales: emotes, medallas, stickers)
@@ -89,7 +104,9 @@ CREATE TABLE coleccion (
     producto_id INT NOT NULL,
     UNIQUE(usuario_id, producto_id),
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id) ON DELETE CASCADE,
+    --Es lo que une colección (inventario) con usuario. 1:1
     FOREIGN KEY (producto_id) REFERENCES tienda(producto_id) ON DELETE CASCADE
+    --Es lo que une los productos con la colección. N:1
 );
 
 -- 10. BLOQUEADOS (Usuarios bloqueados para no ver su contenido o chat)
@@ -99,7 +116,9 @@ CREATE TABLE bloqueados (
     usuario_bloqueado_id INT NOT NULL, -- El bloqueado
     -- UNIQUE(usuario_id, usuario_bloqueado_id),
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id) ON DELETE CASCADE,
+    --Necesitamos la ID del que está bloqueando. 1:1
     FOREIGN KEY (usuario_bloqueado_id) REFERENCES usuarios(usuario_id) ON DELETE CASCADE
+    --Necesitamos la ID del usuario bloqueado N:1
 );
 
 -- 11. NOTIFICACIÓN
@@ -111,6 +130,7 @@ CREATE TABLE notificacion (
     tipo ENUM('live_en_vivo', 'nuevo_seguidor', 'sistema', 'mensajes_nuevos') NOT NULL,
     creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id) ON DELETE CASCADE
+    --Un usuario puede recibir muchas notificaciones. N:1
 );
 
 -- 12. BILLETERA (Monedas virtuales de la plataforma)
@@ -119,6 +139,7 @@ CREATE TABLE billetera (
     usuario_id INT UNIQUE NOT NULL,
     saldo_monedas INT DEFAULT 0,
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id) ON DELETE CASCADE
+    --Es lo que junta al usuario con su billetera. 1:1
 );
 
 -- 13. REPORTES
@@ -130,5 +151,7 @@ CREATE TABLE reportes (
     estado ENUM('pendiente', 'revisado', 'desestimado') DEFAULT 'pendiente',
     creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (denunciante_id) REFERENCES usuarios(usuario_id) ON DELETE CASCADE,
+    --Necesitamos la ID del que está denunciando. 1:1
     FOREIGN KEY (denunciado_id) REFERENCES usuarios(usuario_id) ON DELETE CASCADE
+    --Necesitamos la ID del que está siendo denunciado. N:1
 );
